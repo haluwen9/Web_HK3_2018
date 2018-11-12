2705ea8451ee4c06571cfeb0664ed26adf04a8b9
Luong Cong Tam <congtam.luong0406@gmail.com>
2018-11-12 22:21:14 +0700

    Add Login/Register Dialog
    Add Login/Register Dialog

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-jQuery(document).ready(function ($) {
-
+jQuery(document).ready(function($) {
     // jQuery sticky Menu
-    $(".mainmenu-area").sticky({
+    $('.mainmenu-area').sticky({
         topSpacing: 0
     });
-
 
     $('.product-carousel').owlCarousel({
         loop: true,
@@ -13,13 +11,13 @@ jQuery(document).ready(function ($) {
         responsiveClass: true,
         responsive: {
             0: {
-                items: 1,
+                items: 1
             },
             600: {
-                items: 3,
+                items: 3
             },
             1000: {
-                items: 5,
+                items: 5
             }
         }
     });
@@ -31,16 +29,16 @@ jQuery(document).ready(function ($) {
         responsiveClass: true,
         responsive: {
             0: {
-                items: 1,
+                items: 1
             },
             600: {
-                items: 2,
+                items: 2
             },
             1000: {
-                items: 2,
+                items: 2
             },
             1200: {
-                items: 3,
+                items: 3
             }
         }
     });
@@ -52,18 +50,16 @@ jQuery(document).ready(function ($) {
         responsiveClass: true,
         responsive: {
             0: {
-                items: 1,
+                items: 1
             },
             600: {
-                items: 3,
+                items: 3
             },
             1000: {
-                items: 4,
+                items: 4
             }
         }
     });
-
-
     // Bootstrap Mobile Menu fix
     // $(".navbar-nav li a").click(function () {
     //     $(".navbar-collapse").removeClass('in');
@@ -87,17 +83,44 @@ jQuery(document).ready(function ($) {
     // })
 });
 
-(function (i, s, o, g, r, a, m) {
+(function(i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
+    (i[r] =
+    i[r] ||
+    function() {
+        (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
     a.async = 1;
     a.src = g;
-    m.parentNode.insertBefore(a, m)
-})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    m.parentNode.insertBefore(a, m);
+})(
+    window,
+    document,
+    'script',
+    'https://www.google-analytics.com/analytics.js',
+    'ga'
+);
 
 ga('create', 'UA-10146041-21', 'auto');
 ga('send', 'pageview');
+
+
+// Bông Xù Store
+
+function switchForm(frm) {
+    var lginfrm = $('#login-form');
+    var regfrm = $('#register-form');
+    
+    if (frm === "login") {
+        lginfrm.css('display','');
+        regfrm.css('display','none');
+    }
+    else if (frm === 'register') {
+        lginfrm.css('display','none');
+        regfrm.css('display','');
+    }
+}
+
+// END
