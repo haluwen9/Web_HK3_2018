450359cb57112c50b8f4c35960cd22cc27f9f972
Luong Cong Tam <congtam.luong0406@gmail.com>
2019-01-02 15:53:56 +0700

    rename

--- controllers/home.php
+++ controllers/home.php
@@ -1,3 +1,3 @@
 <?php
-  include_once("views/index.php");
+  include_once("views/homepage.php");
 ?>
--- index.php
+++ index.php
@@ -2,9 +2,4 @@
   session_start();
 
 
-  $homeUrl = $_SERVER['REQUEST_URI'];
-  $asset_folder = $homeUrl."assets";
-  $files_folder = $homeUrl."/files";
-
-
   include_once("controllers/home.php");
